4edc11b60b87953deec6c580bb62b73fe3bd343d
Amin Bashi <amin@leafpoints.com>
2016-09-24 19:05:06 -0400

    Initial Commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Amin Bashi on 2016-09-24.
+//  Copyright © 2016 AminBashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
